32acdb937c5fcd962084633e6fb5f51be25a20dd
soofiajimenez <softweigg@gmail.com>
2025-07-02 14:05:01 +0100

    Update user.py

--- smartstyle_eco/models/user.py
+++ smartstyle_eco/models/user.py
@@ -2,4 +2,4 @@ class User:
     def __init__(self, user_id, name, preferences):
         self.user_id = user_id
         self.name = name
-        self.preferences = preferences  # e.g., preferred colors, styles
+        self.preferences = preferences  # preferred colors, styles
